d895655bf2d0cc914e8e83a25b78b6b015824ba6
sanele23 <sanele23@live.com>
2024-02-18 21:23:51 +0200

    new update

--- controllers/users.js
+++ controllers/users.js
@@ -3,107 +3,132 @@ const mongodb = require("../data/database"); // mongodb connection
 // a variable that mongo will use to create uniqui identifier for each database
 const ObjectId = require("mongodb").ObjectId;
 
+// add validation and error handling
 const getAll = async (req, res) => {
-  // #swagger.tags=['Users']
-  const result = await mongodb.getDatabase().db().collection("users").find();
-  result.toArray().then((users) => {
+  try {
+    // #swagger.tags=['Users']
+    const result = await mongodb.getDatabase().db().collection("users").find();
+    const users = await result.toArray();
+
+    if (!Array.isArray(users)) {
+      throw new Error("Invalid data received from the database");
+    }
+
     res.setHeader("Content-Type", "application/json");
     res.status(200).json(users);
-  });
-};
-
-const getSingle = async (req, res) => {
-  // #swagger.tags=['Users']
-  const userId = new ObjectId(req.params.id);
-  // constant variable that connects to the database
-  const result = await mongodb
-    .getDatabase()
-    .db()
-    .collection("user_info")
-    .find({ _id: userId });
-  result.toArray().then((users) => {
-    res.setHeader("Content-Type", "application/json");
-    res.status(200).json(users);
-  });
-};
-
-const createUser = async (req, res) => {
-  // #swagger.tags=['Users']
-  const user = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    username: req.body.username,
-    email: req.body.email,
-    contactDetails: req.body.contactDetails,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday,
-  };
-  // get id and update according to the user
-  const response = await mongodb
-    .getDatabase()
-    .db()
-    .collection("user_info")
-    .insertOne(user);
-
-  // response
-  if (response.acknowledge) {
-    res.status(204).send();
-  } else {
-    res
-      .status(500)
-      .json(response.error || "Some error occured while updating the user.");
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Error fetching users" });
   }
 };
+
+
+const getSingle = async (req, res) => {
+  try {
+    // #swagger.tags=['Users']
+    const userId = new ObjectId(req.params.id);
+    const result = await mongodb
+      .getDatabase()
+      .db()
+      .collection("user_info")
+      .find({ _id: userId });
+
+    const users = await result.toArray();
+
+    if (!Array.isArray(users) || users.length === 0) {
+      return res.status(404).json({ success: false, message: "User not found" });
+    }
+
+    res.setHeader("Content-Type", "application/json");
+    res.status(200).json(users);
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Error fetching user" });
+  }
+};
+
+
+const createUser = async (req, res) => {
+  try {
+    // #swagger.tags=['Users']
+    const user = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      username: req.body.username,
+      email: req.body.email,
+      contactDetails: req.body.contactDetails,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday,
+    };
+
+    const response = await mongodb
+      .getDatabase()
+      .db()
+      .collection("user_info")
+      .insertOne(user);
+
+    if (response.acknowledged) {
+      res.status(204).send();
+    } else {
+      throw new Error("User insertion failed");
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
+  }
+};
+
 
 // update
 const updateUser = async (req, res) => {
-  // #swagger.tags=['Users']
-  const userId = new ObjectId(req.params.id);
-  const user = {
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
-    username: req.body.username,
-    email: req.body.email,
-    contactDetails: req.body.contactDetails,
-    favoriteColor: req.body.favoriteColor,
-    birthday: req.body.birthday,
-  };
-  // get id and update according to the user
-  const response = await mongodb
-    .getDatabase()
-    .db()
-    .collection("user_info")
-    .replaceOne({ _id: userId }, user);
+  try {
+    // #swagger.tags=['Users']
+    const userId = new ObjectId(req.params.id);
+    const user = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      username: req.body.username,
+      email: req.body.email,
+      contactDetails: req.body.contactDetails,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday,
+    };
 
-  // response
-  if (response.modifiedCount > 0) {
-    res.status(204).send();
-  } else {
-    res
-      .status(500)
-      .json(response.error || "Some error occured while updating the user.");
+    const response = await mongodb
+      .getDatabase()
+      .db()
+      .collection("user_info")
+      .replaceOne({ _id: userId }, user);
+
+    if (response.modifiedCount > 0) {
+      res.status(204).send();
+    } else {
+      throw new Error("User update failed");
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
   }
 };
+
 
 // delete
 const deleteUser = async (req, res) => {
-  // #swagger.tags=['Users']
-  const userId = new ObjectId(req.params.id);
-  const response = await mongodb
-    .getDatabase()
-    .db()
-    .collection("user_info")
-    .deleteUser({ _id: userId });
+  try {
+    // #swagger.tags=['Users']
+    const userId = new ObjectId(req.params.id);
+    const response = await mongodb
+      .getDatabase()
+      .db()
+      .collection("user_info")
+      .deleteOne({ _id: userId });
 
-  // response
-  if (response.deleteCount > 0) {
-    res.status(204).send();
-  } else {
-    res
-      .status(500)
-      .json(response.error || "Some error occured while updating the user.");
+    if (response.deletedCount > 0) {
+      res.status(204).send();
+    } else {
+      throw new Error("User deletion failed");
+    }
+  } catch (error) {
+    res.status(500).json({ success: false, message: error.message });
   }
 };
+
 
 module.exports = {
   getAll,
